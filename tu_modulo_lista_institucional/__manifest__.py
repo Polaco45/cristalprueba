37abb4b8a6e8b462480a6aa0653d7c50c1696c6f
Joacoramello <joaquinramello@gmail.com>
2025-06-12 17:24:00 -0300

    Delete tu_modulo_lista_institucional directory

--- tu_modulo_lista_institucional/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Cambio automático a Lista Institucional',
-    'version': '1.0',
-    'depends': ['base', 'sale'],
-    'author': 'Química Cristal',
-    'category': 'Sales',
-    'installable': True,
-    'application': False,
-}
