b179ca508829422f7d0cb5eb9ef4e88747822f8f
Joacoramello <joaquinramello@gmail.com>
2025-06-12 17:18:01 -0300

    Create __manifest__.py

---
+++ tu_modulo_lista_institucional/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Cambio automático a Lista Institucional',
+    'version': '1.0',
+    'depends': ['base', 'sale'],
+    'author': 'Química Cristal',
+    'category': 'Sales',
+    'installable': True,
+    'application': False,
+}
