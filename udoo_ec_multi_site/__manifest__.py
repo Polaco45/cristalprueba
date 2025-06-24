5c32820f789bc25a6fc883de1d167e4a66d7304d
Joacoramello <joaquinramello@gmail.com>
2025-06-24 12:16:04 -0300

    Update __manifest__.py

--- udoo_ec_multi_site/__manifest__.py
+++ udoo_ec_multi_site/__manifest__.py
@@ -1,28 +1,31 @@
-# -*- coding: utf-8 -*-
-# Copyright 2024 Sveltware Solutions
-
-{
-    'name': 'Udoo Multi-Company Product',
-    'category': 'Website/Website',
-    'summary': 'Manage multichannel listing eCommerce product with bulk website assign, multi website product, multi website category, mass update publishing state, product multi website, managing products across multiple websites, multi shop, multi product, multi categories, multiple websites sale per product, odoo multi websites, multi website selection for products | multiple websites per category | bulk website assign | product multiple website',
-    'version': '1.0.2',
-    'license': 'OPL-1',
-    'author': 'Sveltware Solutions',
-    'website': 'https://www.linkedin.com/in/sveltware',
-    'live_test_url': 'https://omux.sveltware.com/web/login',
-    'images': ['static/description/banner.png'],
-    'depends': [
-        'website_sale',
-    ],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/product_views.xml',
-        'views/multi_website_product.xml',
-        'wizard/multi_website_setter.xml',
-    ],
-    'assets': {
-        'web.assets_backend': [
-            'udoo_ec_multi_site/static/src/**/*',
-        ],
-    },
-}
+ {
+     'name': 'Udoo Multi-Company Product',
+     'category': 'Website/Website',
+     'summary': 'Manage multichannel listing eCommerce product ...',
+     'version': '1.0.2',
+     'license': 'OPL-1',
+     'author': 'Sveltware Solutions',
+     'website': 'https://www.linkedin.com/in/sveltware',
+     'live_test_url': 'https://omux.sveltware.com/web/login',
+     'images': ['static/description/banner.png'],
+-    'depends': [
+-        'website_sale',
+-    ],
++    'depends': [
++        'website_sale',
++        'product',           # necesario para product.pricelist
++        'website',           # para heredar multi.website.mixin
++    ],
+     'data': [
+         'security/ir.model.access.csv',
+         'views/product_views.xml',
+         'views/multi_website_product.xml',
+         'wizard/multi_website_setter.xml',
++        'views/product_pricelist_views.xml',  # vista para website_ids en precios
+     ],
+     'assets': {
+         'web.assets_backend': [
+             'udoo_ec_multi_site/static/src/**/*',
+         ],
+     },
+ }
