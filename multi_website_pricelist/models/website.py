612168ebec0ad1583a2d39cee06d8194ef1190f4
Joacoramello <joaquinramello@gmail.com>
2025-06-24 11:21:49 -0300

    Update website.py

--- multi_website_pricelist/models/website.py
+++ multi_website_pricelist/models/website.py
@@ -1,10 +1,19 @@
-from odoo import models
+from odoo import models, api
 
 class Website(models.Model):
     _inherit = 'website'
 
-    def _get_pricelist(self):
-        # Buscar lista de precios asociada al sitio web actual
-        return self.env['product.pricelist'].search([
-            ('website_ids', 'in', self.id),
-        ], limit=1)
+    @api.model
+    def get_current_pricelist(self):
+        # Si estamos en el frontend con request.website, entramos para ese registro
+        website = self.env['website'].browse(self.env['ir.config_parameter'].sudo().get_param('website.id', default=False)) \
+                  or (self if self._name == 'website' else None)
+        if website:
+            # Buscamos la primera lista que tenga este sitio en website_ids
+            pricelist = self.env['product.pricelist'].search([
+                ('website_ids', 'in', website.id),
+            ], limit=1)
+            if pricelist:
+                return pricelist
+        # Si no encontramos, delegamos a la implementación estándar
+        return super(Website, self).get_current_pricelist()
