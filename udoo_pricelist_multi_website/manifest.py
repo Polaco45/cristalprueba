1b5dc66c6fd0fb832ebf4e091814761f95020bf8
Joacoramello <joaquinramello@gmail.com>
2025-06-24 20:20:18 -0300

    Create manifest.py

---
+++ udoo_pricelist_multi_website/manifest.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Pricelist Multi Website',
+    'version': '1.0.1',
+    'category': 'Website/Ecommerce',
+    'summary': 'Allow multi-website assignment for product pricelists and partner-specific pricelist',
+    'license': 'OPL-1',
+    'author': 'Custom',
+    'website': 'https://tu-dominio.com',
+    'depends': [
+        'website_sale',
+        'product',
+        'website',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_pricelist_views.xml',
+    ],
+    'application': False,
+    'installable': True,
+    'auto_install': False,
+}
