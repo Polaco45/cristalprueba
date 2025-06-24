42517391b190c14cd5cb9e71d14f1a190d855e96
Joacoramello <joaquinramello@gmail.com>
2025-06-24 17:31:50 -0300

    Update website.py

--- udoo_pricelist_multi_website/models/website.py
+++ udoo_pricelist_multi_website/models/website.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+from odoo import models, api
 
 class Website(models.Model):
     _inherit = 'website'
 
     def get_current_pricelist(self):
-        """ Sobrescribimos para usar public_website_ids en lugar de website_id """
         self.ensure_one()
         return self.env['product.pricelist'].search(
             ['|',
              ('public_website_ids', '=', False),
              ('public_website_ids', 'in', self.id)],
             order='sequence asc',
-            limit=1
+            limit=1,
         )
