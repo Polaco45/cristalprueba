6b6d95783349e16b232d3e35cec0f64e231a7b00
Joacoramello <joaquinramello@gmail.com>
2025-06-24 12:26:09 -0300

    Update product_pricelist.py

--- udoo_ec_multi_site/models/product_pricelist.py
+++ udoo_ec_multi_site/models/product_pricelist.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+from odoo import api, fields, models
 
 class ProductPricelist(models.Model):
-    _inherit = [
-        'product.pricelist',      # el modelo original de Odoo
-        'multi.website.mixin',    # nuestro mixin para website_ids
-    ]
-    # Con esto el campo website_ids y su lógica compute/inverse
-    # ya quedan asociados a product.pricelist.
+    _inherit = 'product.pricelist'
+
+    # Campo M2M para asignar múltiples sitios web
+    public_website_ids = fields.Many2many(
+        string='Websites',
+        comodel_name='website',
+        relation='product_pricelist_public_website_rel',
+        column1='pricelist_id',
+        column2='website_id',
+    )
+
+    @api.model
+    def _search_get_detail(self, website, order, options):
+        """ Si public_website_ids está vacío, aparece en todos los sitios;
+            si no, sólo en los sitios seleccionados. """
+        res = super()._search_get_detail(website, order, options)
+        res['base_domain'].append('|')
+        res['base_domain'].append(('public_website_ids', '=', False))
+        res['base_domain'].append(('public_website_ids', 'in', website.ids))
+        return res
