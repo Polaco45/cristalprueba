c406e4c0e32db47f7094ca30025af8ffaf930b71
Joacoramello <joaquinramello@gmail.com>
2025-06-24 12:31:27 -0300

    Update __init__.py

--- udoo_ec_multi_site/models/__init__.py
+++ udoo_ec_multi_site/models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-# Copyright 2024 Sveltware Solutions
-
+from . import multi_website_mixin
 from . import product_template
 from . import product_public_category
 from . import multi_website_product
+# … y el nuevo:
 from . import product_pricelist
