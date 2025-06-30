f9754c77061619a956eb236dcfe9792e8ad51060
Joacoramello <joaquinramello@gmail.com>
2025-06-30 11:03:00 -0300

    Delete payment_delivery_discount_full directory

--- payment_delivery_discount_full/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import payment_provider
-from . import delivery_carrier
-from . import sale_order
--- payment_delivery_discount_full/models/sale_order.py
+++
@@ -1,6 +0,0 @@
-from odoo import models
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-    # La lógica se define en el controlador del sitio web
-    pass
