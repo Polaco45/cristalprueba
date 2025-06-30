dbede19e08ee0c05cf3a488ec52e287dec68ee30
Felimartinez1 <felimartinez072@gmail.com>
2025-06-30 14:18:18 -0300

    fix

--- chatbot_whatsapp/models/chat_memory.py
+++ chatbot_whatsapp/models/chat_memory.py
@@ -1,18 +1,21 @@
 from odoo import models, fields, api
 from datetime import timedelta
 
-class ChatbotWhatsappMemory(models.Model):
+class WhatsAppMemory(models.Model):
     _name = 'chatbot.whatsapp.memory'
-    _description = 'Memoria chatbot WhatsApp'
+    _description = 'Memoria del chatbot de WhatsApp'
 
-    partner_id = fields.Many2one('res.partner', string='Partner', required=True)
-    flow_state = fields.Char(string='Flow State')
-    last_intent_detected = fields.Char(string='Última intención detectada')
-    last_variant_id = fields.Many2one('product.product', string='Última variante seleccionada')
-    last_qty_suggested = fields.Integer(string='Última cantidad sugerida')
-    data_buffer = fields.Text(string='Datos temporales JSON')
+    phone = fields.Char(index=True)
+    partner_id = fields.Many2one('res.partner')
 
+    last_intent_detected = fields.Char()  # 🆕 Intención global NLP
+    flow_state = fields.Char()  # 🆕 Estado actual del flujo conversacional
+
+    last_variant_id = fields.Many2one('product.product')
+    last_qty_suggested = fields.Integer()
+    data_buffer = fields.Text()
     timestamp = fields.Datetime(string='Timestamp', default=fields.Datetime.now)  # CORREGIDO
+
     @api.model
     def clean_old_memory(self):
         expired_time = fields.Datetime.now() - timedelta(minutes=30)
