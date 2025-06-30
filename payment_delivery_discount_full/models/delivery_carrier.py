67100f8d5f0f6346008b6304f15030a51f876447
Joacoramello <joaquinramello@gmail.com>
2025-06-30 11:37:09 -0300

    Delete payment_delivery_discount_full directory

--- payment_delivery_discount_full/models/delivery_carrier.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class DeliveryCarrier(models.Model):
-    _inherit = 'delivery.carrier'
-
-    discount_percent = fields.Float(string="Descuento (%) para la web")
-    discount_product_id = fields.Many2one('product.product', string="Producto de descuento")
--- payment_delivery_discount_full/models/payment_provider.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class PaymentProvider(models.Model):
-    _inherit = 'payment.provider'
-
-    discount_percent = fields.Float(string="Descuento (%) para la web")
-    discount_product_id = fields.Many2one('product.product', string="Producto de descuento")
