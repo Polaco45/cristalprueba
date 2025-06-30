6ef2a3c640676c5372af7b8608532136f7a72951
Felimartinez1 <felimartinez072@gmail.com>
2025-06-30 13:18:49 -0300

    update fields

--- chatbot_whatsapp/models/chat_memory.py
+++ chatbot_whatsapp/models/chat_memory.py
@@ -7,14 +7,18 @@ class WhatsAppMemory(models.Model):
 
     phone = fields.Char(index=True)
     partner_id = fields.Many2one('res.partner')
-    last_intent = fields.Char()
-    data_buffer = fields.Text()
+
+    last_intent = fields.Char()  # [DEPRECATED] Reemplazado por `last_intent_detected`
+    last_intent_detected = fields.Char()  # 🆕 Intención global NLP
+    flow_state = fields.Char()  # 🆕 Estado actual del flujo conversacional
+
     last_variant_id = fields.Many2one('product.product')
     last_qty_suggested = fields.Integer()
+    data_buffer = fields.Text()
     timestamp = fields.Datetime(auto_now_add=True)
 
     @api.model
     def clean_old_memory(self):
-        expired_time = fields.Datetime.now() - timedelta(minutes=30)    
+        expired_time = fields.Datetime.now() - timedelta(minutes=30)
         old = self.sudo().search([('timestamp', '<', expired_time)])
         old.unlink()
