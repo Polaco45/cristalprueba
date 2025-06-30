ad2bcd5ea0b9344ea49d4643ec9f16ed06f9fedc
Joacoramello <joaquinramello@gmail.com>
2025-06-30 11:02:20 -0300

    Delete payment_delivery_discount_web directory

--- payment_delivery_discount_web/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import sale_order, payment_acquirer, delivery_carrier
--- payment_delivery_discount_web/models/payment_acquirer.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class PaymentAcquirer(models.Model):
-    _inherit = 'payment.acquirer'
-
-    discount_percent = fields.Float("Descuento (%)")
