b0b7935387ddbedbaf25055cba0ec2720ad6f2ab
Joacoramello <joaquinramello@gmail.com>
2025-06-30 12:24:47 -0300

    Update __manifest__.py

--- payment_delivery_discount_definitivo/__manifest__.py
+++ payment_delivery_discount_definitivo/__manifest__.py
@@ -1,13 +1,15 @@
+# __manifest__.py
 {
     'name': "Descuento por método de pago y entrega",
     'version': "1.0",
-    'category': 'Website',
+    'category': "Website",
     'summary': "Aplica descuentos por método de pago y transporte en el e-commerce",
     'depends': [
-        'website_sale',
-        'delivery',
-        'payment',
-        'sale',
+        'sale',              # para sale.order
+        'delivery',          # para delivery.carrier.form
+        'payment',           # para payment.provider.form
+        'website_sale',      # si luego extiendes algo en la web
+        'website_sale_collect',  # para la vista de “In-store Delivery Carrier Form”
     ],
     'data': [
         'views/delivery_carrier_views.xml',
