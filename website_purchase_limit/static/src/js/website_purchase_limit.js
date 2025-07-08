b1e123d953c05ad9d49e253ee5bb6c3a2f7d62e9
Joacoramello <joaquinramello@gmail.com>
2025-07-08 10:31:41 -0300

    Update website_purchase_limit.js

--- website_purchase_limit/static/src/js/website_purchase_limit.js
+++ website_purchase_limit/static/src/js/website_purchase_limit.js
@@ -6,34 +6,32 @@ publicWidget.registry.websiteLimit = publicWidget.Widget.extend({
     events: {
         'click a[name="website_sale_main_button"]': '_onCheckoutButtonClick',
     },
-    /**
-     * When the total amount is less than the limit applied in the configuration settings, an error popup window appears.
-     *
-     * @override
-     */
+
     init() {
         this._super(...arguments);
     },
-//    Method to check the purchase minimum limit
+
     _onCheckoutButtonClick(event) {
         if (this.$el.find("#website_purchase_limit_value").length) {
             event.preventDefault();
             const limit = parseFloat(this.$el.find("#website_purchase_limit_value").attr("limit"));
             const open_deactivate_modal = true;
-            // HTML modal structure
+
             const modalHTML = `
             <div class="modal ${open_deactivate_modal ? 'show d-block' : ''}" id="popup_error_message" tabindex="-1" role="dialog">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5>The purchase amount did not meet the limit!</h3>
+                            <h5>¡El monto mínimo no fue alcanzado!</h5>
                             <button type="button" class="btn-close" data-dismiss="modal"></button>
                         </div>
                         <form class="modal-body" role="form">
-                            The purchase amount must be greater than the limit
-                            <b>
-                                <span>${limit}</span>
-                            </b>
+                            <p>Para continuar con la compra, el total debe ser mayor a 
+                                <b>$${limit.toLocaleString('es-AR')}</b>.
+                            </p>
+                            <p style="font-size: 0.85rem; color: #555;">
+                                Si querés hacer una compra menor, <strong>contactanos por WhatsApp</strong> y vamos a ayudarte personalmente 😊
+                            </p>
                         </form>
                     </div>
                 </div>
@@ -45,4 +43,4 @@ publicWidget.registry.websiteLimit = publicWidget.Widget.extend({
             });
         }
     }
-})
+});
