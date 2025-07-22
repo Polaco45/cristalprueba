7ff659abb9b6de92806716a9b7073d4aa5a43134
Felimartinez1 <felimartinez072@gmail.com>
2025-07-21 21:48:25 -0300

    mat .

--- chatbot_whatsapp/__manifest__.py
+++ chatbot_whatsapp/__manifest__.py
@@ -26,8 +26,8 @@
     'assets': {
         # for everything in the mail/thread/discuss view:
         'web.assets_backend': [
-            'chatbot_whatsapp/static/src/js/chatbot_toggle_button.js',
-            'chatbot_whatsapp/static/src/xml/discuss_header_extend.xml',
+            'static/src/js/chatbot_toggle_button.js',
+            'static/src/xml/discuss_header_extend.xml',
         ],
     },
     'installable': True,
