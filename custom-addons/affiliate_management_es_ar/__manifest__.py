ff72ab6bfdf7e9b2d887b25f4897957c28781656
Polaco45 <94990156+Polaco45@users.noreply.github.com>
2025-08-05 10:46:29 -0300

    i18n: es_AR for Webkul Affiliate Management

---
+++ custom-addons/affiliate_management_es_ar/__manifest__.py
@@ -0,0 +1,7 @@
+{
+    "name": "Affiliate Management – Spanish (Argentina)",
+    "version": "1.0.0",
+    "depends": ["affiliate_management", "wk_wizard_messages"],
+    "installable": True,
+    "application": False
+}
